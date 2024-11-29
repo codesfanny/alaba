dd5142d61adcbb531d54541ad4e94d7565f70b22
Felix <felixanufanny@gmail.com>
2024-11-29 14:53:26 +0100

    Fix problem with login function

--- api/authentication.js
+++ api/authentication.js
@@ -2,7 +2,7 @@ import customInstance from "../src/axiosInstance";
 
 const AuthenticationAPI = () => {
   const login = async (values) => {
-    const response = await customInstance.post("/api/v1/auth/login", values);
+    const response = await customInstance.post("/login", values);
     return response;
   };
   return {
